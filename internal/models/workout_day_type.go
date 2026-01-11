657cc57b39056a5ddd4131663b9c35ec4df1acaa
Saenko Dmitry <dmitry.saenko@aliexpress.ru>
2026-01-11 16:49:04 +0300

    fix comment

--- internal/models/workout_day_type.go
+++ internal/models/workout_day_type.go
@@ -6,7 +6,7 @@ type WorkoutDayType struct {
 	ID               int64 `gorm:"primaryKey"`
 	WorkoutProgramID int64
 	Name             string
-	Preset           string // id_1:reps_0*weight_0,reps_1*weight_1;id_2:reps_2_0*weight_2_0;... // 3:17*100,15*160,12*200,12*240,12*260;2:14*40,14*40,14*45,14*50
+	Preset           string
 	CreatedAt        time.Time
 }
 
